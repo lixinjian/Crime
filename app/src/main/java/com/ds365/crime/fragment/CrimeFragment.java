49cfed06c1adab8a36c763977d3abbeec61453eb
lixinjian <xinjian.li@ds365.com>
2017-07-13 18:19:16 +0800

    fd

--- app/src/main/java/com/ds365/crime/fragment/CrimeFragment.java
+++ app/src/main/java/com/ds365/crime/fragment/CrimeFragment.java
@@ -232,13 +232,6 @@ public class CrimeFragment extends Fragment {
                 .updateCrime(mCrime);
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        CrimeLab.getCrimeLab(getActivity())
-                .updateCrime(mCrime);
-    }
-
     private void updateDate() {
         DateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd");
         mDateButton.setText(dateFormater.format(mCrime.getDate()));
